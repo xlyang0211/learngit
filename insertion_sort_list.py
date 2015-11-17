bc2dacc820e931eee2bc437c46fc7600a4cb0c4d
xlyang0211 <xlyang0211@126.com>
2015-11-17 14:44:33 +0800

    realize sorting a linked-list using insertion sort.

--- insertion_sort_list.py
+++ insertion_sort_list.py
@@ -1,3 +1,9 @@
+# This algorithm realizes sort a linked list using insert sort;
+# And the hard nut is:
+# to locate the position to insert into, you need to move index every time, and the head may change after every
+# insertion;
+
+
 # Definition for singly-linked list.
 class ListNode(object):
     def __init__(self, x):
@@ -10,12 +16,22 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        count = 0
+        count = 1
         front_pointer = head
         current_pointer = head.next
         while current_pointer is not None:
-            self.insert(head, count, front_pointer, current_pointer)
+            if current_pointer.val < front_pointer.val:
+                head = self.insert(head, count, front_pointer, current_pointer)
+            # position may change and this will not work!
+            # front_pointer = front_pointer.next
+            # current_pointer = current_pointer.next
             count += 1
+            flag_front_pointer = 0
+            front_pointer = head
+            while flag_front_pointer != count - 1:
+                front_pointer = front_pointer.next
+                flag_front_pointer += 1
+            current_pointer = front_pointer.next
         return head
 
     def insert(self, head, count, front_pointer, current_pointer):
@@ -32,20 +48,26 @@ class Solution(object):
                     front_pointer.next = current_pointer.next
                     pointer_ahead.next = current_pointer
                     current_pointer.next = pointer
+                break
             if new_count == 0:
                 pointer = pointer.next
             else:
                 pointer_ahead = pointer_ahead.next
                 pointer = pointer.next
             new_count += 1
+        return head
 
 if __name__ == "__main__":
     head = ListNode(3)
     head.next = ListNode(4)
-    head.next = ListNode(1)
-    head.next = ListNode(2)
+    head.next.next = ListNode(1)
+    head.next.next.next = ListNode(2)
+    head.next.next.next.next = ListNode(0)
     solution = Solution()
     head = solution.insertionSortList(head)
-    print head.val, head.next.val, head.next.next.val, head.next.next.next.val
+    pointer = head
+    while pointer is not None:
+        print pointer.val
+        pointer = pointer.next
 
 
